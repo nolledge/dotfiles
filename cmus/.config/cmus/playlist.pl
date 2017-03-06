87290dd99447ec241d5f621373a408e1831f91e1
Christof Nolle <mail@christof-nolle.de>
2017-03-06 23:00:08 +0100

    to add yaourt config and to remove cmus files

--- cmus/.config/cmus/playlist.pl
+++
@@ -1,7 +0,0 @@
-/home/christof/libs/music_sorted/massive_attack/1991-blue_lines/01-02-one_love.mp3
-/home/christof/libs/music_sorted/massive_attack/1991-blue_lines/01-07-daydreaming.mp3
-/home/christof/libs/music_sorted/massive_attack/1991-blue_lines/01-08-lately.mp3
-/home/christof/libs/music_sorted/massive_attack/1991-blue_lines/01-09-hymn_of_the_big_wheel.mp3
-/home/christof/libs/music_sorted/massive_attack/1991-blue_lines/01-09-hymn_of_the_big_wheel.mp3
-/home/christof/libs/music_sorted/massive_attack/1991-blue_lines/01-09-hymn_of_the_big_wheel.mp3
-/home/christof/libs/music_sorted/massive_attack/1991-blue_lines/01-09-hymn_of_the_big_wheel.mp3
